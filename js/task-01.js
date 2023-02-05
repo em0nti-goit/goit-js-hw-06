f83694beb7ed1966e72262e03c61e25f363ca1c7
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-05 22:22:13 +0200

    adds a function makeCategoryLog from task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+const categoriesRef = document.querySelector("#categories");
+const itemsRef = categoriesRef.querySelectorAll(".item");
+
+console.log(`Number of categories: ${itemsRef.length}\n`);
+
+function makeCategoryLog(refs) {
+  for (const ref of refs) {
+    const headerRef = ref.querySelector("h2");
+    const categoriesItems = ref.querySelector("ul").children;
+    console.log(
+      `Category: ${headerRef.textContent}\nElements: ${categoriesItems.length}\n\n`
+    );
+  }
+}
+
+makeCategoryLog(itemsRef);
