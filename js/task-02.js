7ad0215b6a3ec740f4a88bbd8e9c986af7d975b5
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-05 22:54:07 +0200

    complete task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+function createListContent(arr) {
+  return arr.map((elem) => {
+    let node = document.createElement("li");
+    node.className = "item";
+    node.textContent = elem;
+    return node;
+  });
+}
+
+const list = document.querySelector("#ingredients");
+list.append(...createListContent(ingredients));
