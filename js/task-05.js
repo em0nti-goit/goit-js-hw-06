32a2b05ef7340d94db8b6724f399992e57f0e63b
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-06 13:52:19 +0200

    complete task 5

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", textInputHandler);
+
+function textInputHandler() {
+  outputRef.textContent = inputRef.value ? inputRef.value : "Anonymous";
+}
