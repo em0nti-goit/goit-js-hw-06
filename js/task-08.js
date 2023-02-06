e619d1ee6652faf9cabb3cb06e7af48a60b2c15f
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-06 15:48:13 +0200

    complete task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector("form.login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formEl = event.currentTarget.elements;
+  if (formEl.email.value && formEl.password.value) {
+    const formData = {
+      [formEl.email.name]: formEl.email.value,
+      [formEl.password.name]: formEl.password.value,
+    };
+    console.log(formData);
+    formRef.reset();
+  } else {
+    alert("All fields must be filled in");
+  }
+}
