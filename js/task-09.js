3af4ea6a6adeeb2d27f8954b230d9bf489c8bc95
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-06 16:07:22 +0200

    complete task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const bodyRef = document.body;
+const widgetRef = document.querySelector(".widget");
+const buttonChangeColorRef = widgetRef.lastElementChild;
+const spanColorRef = widgetRef.querySelector("span.color");
+
+buttonChangeColorRef.addEventListener("click", onClickChangeColorButton);
+
+function onClickChangeColorButton() {
+  const randomColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = randomColor;
+  spanColorRef.textContent = randomColor;
 }
